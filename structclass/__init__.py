035a6f3f7238aedda405ffb9c5ff88e28e36e39a
subdir <subdir@gmail.com>
2014-09-10 20:25:38 +0400

    Добавлен тест, проверяющий, что декоратор не портит метакласс

--- structclass/__init__.py
+++ structclass/__init__.py
@@ -46,42 +46,52 @@ def struct_patch(name, bases, dict):
 
 
 def struct(cls):
-    '''
-    Декоратор, превращающий класс в структуру.
-    Метакласс не меняется, это полезно, например, если хочется сделать
-    структуру абстрактным базовым классом.
+    ''' Декоратор, превращающий класс в структуру.
+        Метакласс не меняется, это полезно, например, если хочется сделать
+        структуру абстрактным базовым классом.
 
-    >>> @struct
-    ... class Test: pass
-    Traceback (most recent call last):
-    ...
-    AssertionError: 'Test' must be derived from 'object'
-    >>> @struct
-    ... class Test(object):
-    ...     def __init__(self, a, b=1, c=None):
-    ...         if a is None:
-    ...             self.a = 'undefined'
-    ...     def test(self):
-    ...         return (self.a, self.b, self.c)
-    >>> t = Test('a')
-    >>> t.test()
-    ('a', 1, None)
+        >>> @struct
+        ... class Test: pass
+        Traceback (most recent call last):
+        ...
+        AssertionError: 'Test' must be derived from 'object'
+        >>> @struct
+        ... class Test(object):
+        ...     def __init__(self, a, b=1, c=None):
+        ...         if a is None:
+        ...             self.a = 'undefined'
+        ...     def test(self):
+        ...         return (self.a, self.b, self.c)
+        >>> t = Test('a')
+        >>> t.test()
+        ('a', 1, None)
 
-    СРАВНЕНИЕ
+        СРАВНЕНИЕ
 
-    >>> t > Test('a', b=0)
-    True
-    >>> t > Test('b', b=0)
-    False
+        >>> t > Test('a', b=0)
+        True
+        >>> t > Test('b', b=0)
+        False
 
-    ХЕШИ
+        ХЕШИ
 
-    >>> d = {t : True}
-    >>> t in d
-    True
-    >>> t.a = 'x'
-    >>> t in d
-    False
+        >>> d = {t : True}
+        >>> t in d
+        True
+        >>> t.a = 'x'
+        >>> t in d
+        False
+
+        МЕТАКЛАССЫ
+
+        >>> from abc import ABCMeta
+        >>> @struct
+        ... class TestAbc(object):
+        ...     __metaclass__ = ABCMeta
+        ...     def __init__(self, x, y):
+        ...         pass
+        >>> type(TestAbc) is ABCMeta
+        True
     '''
     assert object in inspect.getmro(cls), "{!r} must be derived from 'object'".format(cls.__name__)
     return total_ordering(getattr(cls, '__metaclass__', type)(*struct_patch(
@@ -131,49 +141,49 @@ class StructType(type):
 
 class Struct(object):
     """
-    >>> class Test(Struct):
-    ...     def __init__(self, a, b=1, c=None):
-    ...         if a is None:
-    ...             self.a = 'undefined'
-    ...     def test(self):
-    ...         return (self.a, self.b, self.c)
-    >>> type(Test) is StructType
-    True
-    >>> t = Test('a')
-    >>> t.test()
-    ('a', 1, None)
-    >>> t.c = 2
-    >>> t.test()
-    ('a', 1, 2)
-    >>> t.d = 3
-    Traceback (most recent call last):
-    ...
-    AttributeError: 'Test' object has no attribute 'd'
-    >>> t = Test(a=None, c=2)
-    >>> t.test()
-    ('undefined', 1, 2)
-    >>> t
-    Test(a='undefined', b=1, c=2)
-    >>> print(repr_struct(t, first_named_attr=1))
-    Test('undefined', b=1, c=2)
+        >>> class Test(Struct):
+        ...     def __init__(self, a, b=1, c=None):
+        ...         if a is None:
+        ...             self.a = 'undefined'
+        ...     def test(self):
+        ...         return (self.a, self.b, self.c)
+        >>> type(Test) is StructType
+        True
+        >>> t = Test('a')
+        >>> t.test()
+        ('a', 1, None)
+        >>> t.c = 2
+        >>> t.test()
+        ('a', 1, 2)
+        >>> t.d = 3
+        Traceback (most recent call last):
+        ...
+        AttributeError: 'Test' object has no attribute 'd'
+        >>> t = Test(a=None, c=2)
+        >>> t.test()
+        ('undefined', 1, 2)
+        >>> t
+        Test(a='undefined', b=1, c=2)
+        >>> print(repr_struct(t, first_named_attr=1))
+        Test('undefined', b=1, c=2)
 
-    НАСЛЕДОВАНИЕ (автонаследование слотов не написано, и не факт что оно нужно)
+        НАСЛЕДОВАНИЕ (автонаследование слотов не написано, и не факт что оно нужно)
 
-    >>> class Test2(Test):
-    ...    def __init__(self, a): pass
-    >>> t2 = Test2(1)
-    >>> isinstance(t2, Test)
-    True
-    >>> t2.a
-    1
-    >>> t2.b
-    Traceback (most recent call last):
-    ...
-    AttributeError: b
-    >>> t2.test()
-    Traceback (most recent call last):
-    ...
-    AttributeError: b
+        >>> class Test2(Test):
+        ...    def __init__(self, a): pass
+        >>> t2 = Test2(1)
+        >>> isinstance(t2, Test)
+        True
+        >>> t2.a
+        1
+        >>> t2.b
+        Traceback (most recent call last):
+        ...
+        AttributeError: b
+        >>> t2.test()
+        Traceback (most recent call last):
+        ...
+        AttributeError: b
     """
     __metaclass__ = StructType
 
@@ -197,16 +207,16 @@ def traverse_class_tree(tree):
 
 def get_base_classes(cls):
     '''
-    >>> class A(object): pass
-    >>> get_base_classes(A) == (object,)
-    True
-    >>> class B(A): pass
-    >>> get_base_classes(B) == (A,)
-    True
-    >>> class C(object): pass
-    >>> class D(B, C): pass
-    >>> get_base_classes(D) == (B, C)
-    True
+        >>> class A(object): pass
+        >>> get_base_classes(A) == (object,)
+        True
+        >>> class B(A): pass
+        >>> get_base_classes(B) == (A,)
+        True
+        >>> class C(object): pass
+        >>> class D(B, C): pass
+        >>> get_base_classes(D) == (B, C)
+        True
     '''
     tree = inspect.getclasstree(inspect.getmro(cls), unique=True)
     for cls0, bases in traverse_class_tree(tree):
